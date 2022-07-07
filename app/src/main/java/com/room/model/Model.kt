fe3cc75ac9d26d0c14064ad35ac93b426af47100
kabir <kabiragamirzaewv@gmail.com>
2022-07-08 01:58:31 +0300

    init commit --- model data tasks

--- app/src/main/java/com/room/model/Model.kt
+++ app/src/main/java/com/room/model/Model.kt
@@ -4,12 +4,17 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "users")
 data class Users(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "name") val name: String?,
     @ColumnInfo(name = "email") val email: String?,
+)
+
+data class Tasks(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "description") val description: String?,
 )
 
 
